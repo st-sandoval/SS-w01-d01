d7b679969f41bc20411ba8aa65c44bc617e54763
st-sandoval <michael.sandoval@smoothstack.com>
2021-02-23 07:20:18 +0000

    fixed count bug

--- com.ss.assignments/src/com/ss/assignments/day01/RandomNum.java
+++ com.ss.assignments/src/com/ss/assignments/day01/RandomNum.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class RandomNum {
 	private int random;
+	private int guessCount=5;
 	int userInput;
 	
 	RandomNum(){
@@ -12,15 +13,23 @@ public class RandomNum {
 	private void generateRandom() {
 		this.random  = (int)(Math.random() * 100);
 		System.out.println("generateRandom: " + this.random);
+		System.out.println("Guess the number 1-100, guesses remaining: " + this.guessCount);
 	}
 
 	public void readInput() {
 		int x;
 		Scanner scanny = new Scanner(System.in);
-		System.out.println("Guess the number 1-100: ");
 		x = scanny.nextInt();
 		this.userInput = x;
 		this.compareNums();
+	}
+	
+	private void checkCount(){
+		System.out.println("Guesses remaining: " + --this.guessCount);
+		if(this.guessCount == 0) {
+			System.out.println("Sorry, you ran out of guesses :(");
+			System.exit(0);
+		}
 	}
 	
 	public void compareNums() {
@@ -28,7 +37,8 @@ public class RandomNum {
 			System.out.println("You were within +-10 of the number: " + this.random);
 			System.exit(0);
 		}else {
-			System.out.println("you were further than +-10, guess again.");
+			System.out.println("You were further than +-10, try again.");
+			this.checkCount();
 			this.readInput();
 		}
 	}
